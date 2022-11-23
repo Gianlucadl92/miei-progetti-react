2f1ef9cc3370477fde9ca6493b5cd483a2882aee
Gianlucadl92 <86296807+Gianlucadl92@users.noreply.github.com>
2022-11-23 13:10:17 +0100

    progetti-react

---
+++ dark-mode/src/App.js
@@ -0,0 +1,11 @@
+import DarkMode from "./DarkMode";
+
+function App() {
+  return (
+    <div className="App">
+      <DarkMode />
+    </div>
+  );
+}
+
+export default App;
---
+++ markdown/src/App.js
@@ -0,0 +1,11 @@
+import Markdown from "./Markdown";
+
+function App() {
+  return (
+    <div className="App">
+      <Markdown />
+    </div>
+  );
+}
+
+export default App;
